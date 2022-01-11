bc440db5717028753f3ecb5420373e271a538ff0
Yasin Çalışkan <yasincaliskandev@gmail.com>
2022-01-12 00:32:59 +0300

    feat: Initialize file structure

--- frontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./App.scss";
-
-function App() {
-  return <div className={"app"}>{"Hi!"}</div>;
-}
-
-export default App;
---
+++ frontend/src/component/page/Page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface PageProps {
+  children: React.ReactNode;
+}
+
+function Page({children}: PageProps) {
+  return <div className={"page"}>{children}</div>;
+}
+
+export default Page;
