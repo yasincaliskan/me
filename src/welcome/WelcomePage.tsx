72145fc6d3cca9c50cd333016af251a47a56bc5f
Yasin Çalışkan <yasincaliskandev@gmail.com>
2022-01-16 01:54:56 +0300

    build(typography): Add Google Fonts and typographies

--- src/welcome/WelcomePage.tsx
+++ src/welcome/WelcomePage.tsx
@@ -1,7 +1,15 @@
 import Page from "../component/page/Page";
 
 function WelcomePage() {
-  return <Page>{"Hi, welcome!"}</Page>;
+  return (
+    <Page>
+      <h2 className={"typography--h2"}>{"Hi, I'm Yasin Çalışkan!"}</h2>
+
+      <p className={"typography--body"}>
+        {"I'm software developer and working at Hipo."}
+      </p>
+    </Page>
+  );
 }
 
 export default WelcomePage;
